06429bbd31196be57e898f7b6cbea4179353d450
Carson Davis <carsonreiddavis@gmail.com>
2023-09-13 16:40:21 -0500

    break into functions and extract all fields

--- exporter.py
+++ exporter.py
@@ -3,41 +3,86 @@ from bs4 import BeautifulSoup
 
 BASE_URL = "https://www.goodreads.com/review/list/142587487-random-programmer?page={}&print=true&shelf=read&view=table"
 
+FIELDS = {
+    "title": "title",
+    "author": "author",
+    "isbn": "isbn",
+    "isbn13": "isbn13",
+    "asin": "asin",
+    "num_pages": "num_pages",
+    "avg_rating": "avg_rating",
+    "num_ratings": "num_ratings",
+    "date_pub": "date_pub",
+    "date_pub_edition": "date_pub_edition",
+    "rating": "rating",  # this isn't working
+    "shelves": "shelves",
+    "review": "review",
+    "notes": "notes",
+    "comments": "comments",
+    # "votes": "votes",
+    "read_count": "read_count",
+    "date_started": "date_started",
+    "date_read": "date_read",
+    "date_added": "date_added",
+    "owned": "owned",
+}
+
+RATING_MAPPER = {
+    "did not like it": "1",
+    "it was ok": "2",
+    "liked it": "3",
+    "really liked it": "4",
+    "it was amazing": "5",
+}
+
+
+def field_converter(field_name):
+    """adds the word field in front of the field name"""
+    return f"field {field_name}"
+
+
+def extract_field(row, field_name):
+    """gets the value of a desired field and handles a few special cases"""
+    field_class = field_converter(field_name)
+    div = row.find("td", class_=field_class).div
+
+    # Special case for date_read as it has an additional span element
+    if field_class == "field date_read":
+        span = div.find("span", class_="date_read_value")
+        return span.get_text(strip=True) if span else None
+
+    value = div.get_text(strip=True)
+    if field_class == "field rating":
+        value = RATING_MAPPER.get(value, value)
+
+    return value
+
 
 def get_book_data(page_number):
     response = requests.get(BASE_URL.format(page_number))
     soup = BeautifulSoup(response.content, "html.parser")
 
-    titles = []
-    dates_read = []
+    books = []
 
     # Find all rows with book data
     rows = soup.find_all("tr", class_="bookalike review")
 
     for row in rows:
-        # Get the title
-        title_div = row.find("td", class_="field title").div
-        title = title_div.a.get_text(strip=True)
-        titles.append(title)
+        book = {}
+        for field, class_name in FIELDS.items():
+            book[field] = extract_field(row, class_name)
+        books.append(book)
 
-        # Get the read date
-        date_read_div = row.find("td", class_="field date_read").div
-        date_read_element = date_read_div.find("span", class_="date_read_value")
-        date_read = None
-        if date_read_element:
-            date_read = date_read_element.get_text(strip=True)
-        dates_read.append(date_read)
-
-    return titles, dates_read
+    return books
 
 
 # Assuming there are 10 pages to loop through
-all_titles = []
-all_dates_read = []
+all_books = []
 for i in range(1, 2):  # Replace 11 with the actual number of pages + 1
-    titles, dates_read = get_book_data(i)
-    all_titles.extend(titles)
-    all_dates_read.extend(dates_read)
+    all_books.extend(get_book_data(i))
 
-for title, date in zip(all_titles, all_dates_read):
-    print(f"Title: {title}, Date Read: {date}")
+# for book in all_books:
+#     print(", ".join([f"{key.capitalize()}: {value}" for key, value in book.items()]))
+
+for key, value in all_books[0].items():
+    print(f"{key}: {value}")
